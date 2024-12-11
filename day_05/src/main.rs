1d5498b26e1cc90fabfa68611d5f29cccf282866
Sowmiya Ravikumar <sowmiyathilakavathi@gmail.com>
2024-12-11 21:08:36 +0000

    Day 10 solution

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,3 +1,64 @@
+use std::{collections::HashMap, fs::read_to_string};
+
 fn main() {
     println!("Welcome to Day 5!");
+
+    let (rule_map, page_updates) = read_input_file("./src/data/small_sample.txt");
+
+    for update in page_updates {
+        let mut valid: bool = false;
+        for page in update {
+            if rule_map.contains_key(&page) {
+                let value = rule_map.get(&page);
+                let key = page;
+
+                let idx_2 = if let Some(val) = value {
+                    update.iter().position(|v| v == val); // We borrow `update` here
+                };
+            
+                // `update` is still available here because we only borrowed it with `.iter()`
+                let idx_1 = update.iter().position(|v| v == &key);
+
+                
+                match (idx_1, idx_2) {
+                    (Some(i1), Some(i2)) => {
+                        if i1 < i2 {
+                            println!("idx_1 is less than idx_2: {} < {}", i1, i2);
+                        } else if i1 > i2 {
+                            println!("idx_1 is greater than idx_2: {} > {}", i1, i2);
+                        } else {
+                            println!("idx_1 is equal to idx_2: {} == {}", i1, i2);
+                        }
+                    },
+                    _ => {
+                        println!("At least one of the indices was not found (None).");
+                    }
+                }
+            }
+        }
+        println!("{}", valid)
+    }
+}
+
+fn read_input_file(file_path: &str) -> (HashMap<u8, u8>, Vec<Vec<u8>>) {
+    let file_content = read_to_string(file_path).expect("Failed to open file...");
+
+    let mut rule_map: HashMap<u8, u8> = HashMap::new();
+    let mut page_updates: Vec<Vec<u8>> = Vec::new();
+
+    for line in file_content.lines() {
+        if line.contains("|") {
+            let pages: Vec<u8> = line.split("|").map(|page| page.parse::<u8>().unwrap()).collect();
+
+            if pages.len() == 2 {
+                rule_map.insert(pages[0], pages[1]);
+                // rule_map.insert(pages[1], pages[0]);
+            }
+            
+        } else if line.contains(",") {
+            let updates: Vec<u8> = line.split(",").map(|update| update.parse().unwrap()).collect();
+            page_updates.push(updates);
+        }
+    }
+    return (rule_map, page_updates)
 }
--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,3 +1,60 @@
+use std::{collections::HashSet, fs::read_to_string, vec};
+
 fn main() {
     println!("Welcome to Day 10!");
+
+    // let map: Vec<Vec<u8>> = vec![vec![0, 1, 2, 3], vec![1, 2, 3, 4], vec![8, 7, 6, 5], vec![9, 8, 7, 6]];
+
+    let mut map: Vec<Vec<u8>> = Vec::new();
+
+    let content = read_to_string("./src/sample.txt").expect("Couldn't open the file");
+
+    for line in content.lines() {
+        let row: Vec<u8> = line.chars()
+        .map(|el| el.to_digit(10).unwrap_or(10) as u8)
+        .collect();
+        map.push(row);
+    }
+
+    let rows = map.len();
+    let cols = map[0].len();
+
+    let mut sum = 0;
+
+    for i in 0..rows {
+        for j in 0..cols {
+            if map[i][j] == 0 {
+                let mut nodes: HashSet<(usize, usize)> = HashSet::new();
+                recursive_sum(i, j, rows, cols, &map, 0, &mut nodes);
+                sum += nodes.len();
+            }
+        }
+    }
+
+    println!("sum {}", sum);
+
+}
+
+fn recursive_sum(i: usize, j: usize, rows: usize, cols: usize, map: &Vec<Vec<u8>>, last: u8, nodes: &mut HashSet<(usize, usize)>) {
+    if map[i][j] == 9 && !nodes.contains(&(i, j)){
+        nodes.insert((i, j));
+    }
+
+    // let mut max_sum = sum;
+
+    if j + 1 < cols && map[i][j+1] == last + 1 {
+        recursive_sum(i, j+1, rows, cols, map, last + 1, nodes);
+    } 
+    
+    if i >= 1 && map[i - 1][j] == last + 1 {
+       recursive_sum(i - 1, j, rows, cols, map, last + 1, nodes);
+    } 
+    
+    if j >= 1 && map[i][j - 1] == last + 1 {
+       recursive_sum(i, j - 1, rows, cols, map, last + 1, nodes);
+    } 
+    
+    if i + 1 < rows && map[i + 1][j] == last + 1 {
+       recursive_sum(i + 1, j, rows, cols, map, last + 1, nodes);
+    }
 }
